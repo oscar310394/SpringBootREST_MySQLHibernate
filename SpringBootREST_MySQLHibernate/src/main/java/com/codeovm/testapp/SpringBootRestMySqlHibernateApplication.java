12f417b3345a94c79abaded3e6a7a60389c23988
oscar310394 <oscar310394@gmail.com>
2018-02-13 12:16:06 -0600

    Starting the project

---
+++ SpringBootREST_MySQLHibernate/src/main/java/com/codeovm/testapp/SpringBootRestMySqlHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.codeovm.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestMySqlHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestMySqlHibernateApplication.class, args);
+	}
+}
